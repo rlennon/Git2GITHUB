4f80d30999f5d0a580781ff618f31a1a5134db93
ruth.lennon <ruth.lennon@lyit.ie>
2016-08-09 01:18:39 +0100

    commit this

---
+++ src/pkg/JavaMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pkg;
+
+/**
+ * @author Ruth.Lennon
+ *
+ */
+public class JavaMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello All");
+
+	}
+
+}
